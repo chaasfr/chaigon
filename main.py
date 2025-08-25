53c3ed5cc9eebf7beb41291f822bdb708bc5a020
chaasfr <chaasfr@gmail.com>
2025-08-25 18:11:20 +0200

    first push

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    
+    load_dotenv()
+
+    api_key= os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    resp = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(resp.text)
+    print(f"""
+Prompt tokens: {resp.usage_metadata.prompt_token_count}
+Response tokens: {resp.usage_metadata.candidates_token_count}
+""")
+
+
+if __name__ == "__main__":
+    main()
