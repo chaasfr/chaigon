00a2b6047c267a912f8971badcb1baeeeb3e4b07
chaasfr <chaasfr@gmail.com>
2025-08-25 21:33:15 +0200

    takes question as a prompt

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 
-def main():
+def handle_prompt(prompt):
     
-    load_dotenv()
-
     api_key= os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     resp = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+        contents=prompt)
     print(resp.text)
     print(f"""
 Prompt tokens: {resp.usage_metadata.prompt_token_count}
 Response tokens: {resp.usage_metadata.candidates_token_count}
 """)
 
+def main(args):
+    if len(args) < 2:
+        print("prompt missing!")
+        exit(1)
+    
+    load_dotenv()
+
+    handle_prompt(args[1])
+
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
