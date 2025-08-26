77ed73ff04c6d053753e58c8fa092e7ed00085e3
chaasfr <chaasfr@gmail.com>
2025-08-26 22:07:56 +0200

    feat: added function to parse file content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,6 @@
 import os
+from functions import config
+
 def get_files_info(working_directory, directory=".") -> str:
   results = ['Result for current directory:']  
   directory_full_path = os.path.abspath(os.path.join(working_directory, directory))
@@ -21,3 +23,23 @@ def get_files_info(working_directory, directory=".") -> str:
     return f'Error: {e}'
   
   return "\n".join(results)
+
+def get_file_content(working_directory, file_path) -> str:
+  file_full_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+  if working_directory not in file_full_path:
+    return f'Error: Cannot read "{file_full_path}" as it is outside the permitted working directory'
+  
+  if not os.path.isfile(file_full_path):
+    return f'Error: File not found or is not a regular file: "{file_full_path}"'
+
+  try:
+    with open(file_full_path, "r") as f:
+      result = f.read(config.MAX_CHARS_PER_FILE)
+
+    if len(result) == config.MAX_CHARS_PER_FILE:
+      result += f'\n[...File "{file_full_path}" truncated at 10000 characters]'
+  except Exception as e:
+    return f'Error: {e}'
+ 
+  return result
